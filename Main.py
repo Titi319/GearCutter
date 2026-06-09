799cc64a209a73c4ec4d336f2646dadd3a183ea1
Thierry Liardet <thierry_319@hotmail.com>
2026-06-09 12:59:17 +0200

    Initial commit

---
+++ Main.py
@@ -0,0 +1,13 @@
+from solid2 import *
+
+def generate_gear(m, z):
+    blank = cylinder(r=m*(z+2)/2, h=10)
+    tool = cube([10, 5, 12]) # À remplacer par la forme de dent
+
+    # Logique de soustraction en Python
+    for a in range(-20, 21):
+        blank -= rotate(a)(translate([dist, 0, 0])(tool))
+
+    blank.save_as_scad("gear.scad")
+
+generate_gear(2,25)
